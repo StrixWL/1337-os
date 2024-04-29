7ed5c56bdc7d2d149661f2c29983ede6e85438a9
StrixWL <brahimstrix@gmail.com>
2024-04-29 18:00:25 +0100

    feat(taskbar): add start button with windows logo (style only)

--- src/StrixOS/Taskbar/index.tsx
+++ src/StrixOS/Taskbar/index.tsx
@@ -1,10 +1,15 @@
-import styles from './Taskbar.module.scss';
+import styles from "./Taskbar.module.scss";
+import windowsIcon from "../../assets/win.png";
 
 const Taskbar = () => {
-    return (
-        <div className={styles['Taskbar']}>
-        </div>
-    );
+	return (
+		<div className={styles["Taskbar"]}>
+			<button className={styles["start"]}>
+				<img src={windowsIcon} />
+				Start
+			</button>
+		</div>
+	);
 };
 
 export default Taskbar;
