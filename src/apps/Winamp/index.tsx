6ddd5c6b88c90721001fe432266cae044752eca0
StrixWL <brahimstrix@gmail.com>
2024-06-25 14:56:06 +0100

    feat(winamp): change default theme and add new themes

--- src/apps/Winamp/index.tsx
+++ src/apps/Winamp/index.tsx
@@ -2,31 +2,59 @@ import { useEffect } from "react";
 import Webamp from "webamp";
 
 interface WinampComponent {
-	close?: () => void
+	close?: () => void;
 }
 
-const WinampComponent = ({
-	close
-}: WinampComponent) => {
+const WinampComponent = ({ close }: WinampComponent) => {
 	useEffect(() => {
 		const webamp = new Webamp({
 			initialTracks: [],
-			zIndex: 214748364
+			availableSkins: [
+				{
+					url: "/webamp/themes/Card_Captor_Sakura_-_Purple_Tomoyo.wsz",
+					name: "lain 1",
+				},
+				{
+					url: "/webamp/themes/Iwakura_Lain.wsz",
+					name: "lain 2",
+				},
+				{
+					url: "/webamp/themes/lainamp.ZIP",
+					name: "lain 3 (default)",
+				},
+				{
+					url: "/webamp/themes/Rei_Ayanami_NGE.wsz",
+					name: "rei",
+				},
+				{
+					url: "/webamp/themes/bd3bf6250ffd41012a07e0601bb0fb4c.wsz",
+					name: "-",
+				},
+			],
+			initialSkin: {
+				url: "/webamp/themes/lainamp.ZIP",
+			},
+			zIndex: 214748364,
 		});
-		const webampElem = document.getElementById('winamp') as HTMLDivElement
+		const webampElem = document.getElementById("winamp") as HTMLDivElement;
 		webamp.renderWhenReady(webampElem).then(() => {
-			webampElem.appendChild(document.getElementById('webamp') as HTMLDivElement)
+			webampElem.appendChild(
+				document.getElementById("webamp") as HTMLDivElement
+			);
 		});
 		webamp.onClose(() => {
-			webamp.dispose()
-			close!()
-		})
-	}, [])
+			webamp.dispose();
+			close!();
+		});
+	}, []);
 	return (
-		<div style={{
-			position: 'fixed',
-			visibility: "visible"
-		}} id="winamp"></div>
+		<div
+			style={{
+				position: "fixed",
+				visibility: "visible",
+			}}
+			id="winamp"
+		></div>
 	);
 };
 
